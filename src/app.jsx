a11e8bf2cc69ac62f68615561748b79d971b53bd
yuzexia <327932801@qq.com>
2021-05-02 14:26:39 +0800

    init nav-side

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@
  * @Author: yuze.xia 
  * @Date: 2021-04-29 15:03:27 
  * @Last Modified by: yuze.xia
- * @Last Modified time: 2021-04-29 16:25:01
+ * @Last Modified time: 2021-05-02 14:10:55
  */
 
 import React from 'react';
@@ -19,8 +19,11 @@ class App extends React.Component{
             <Router>
                 <Layout>
                     <Switch>
-                        <Route exact path="/" component={Home}/>
-                        <Redirect from="*" to="/"/>
+                        <Route exact path="/" component={Home} />
+                        <Route to="/product" componetn={Home} />
+                        <Route to="/product.category" componetn={Home} />
+                        <Route to="/order" componetn={Home} />
+                        <Route to="/user" componetn={Home} />
                     </Switch>
                 </Layout>
             </Router>
--- src/component/nav-side/index.jsx
+++ src/component/nav-side/index.jsx
@@ -2,9 +2,10 @@
  * @Author: yuze.xia 
  * @Date: 2021-04-29 16:37:08 
  * @Last Modified by: yuze.xia
- * @Last Modified time: 2021-04-29 17:15:07
+ * @Last Modified time: 2021-05-02 14:24:02
  */
 import React from 'react';
+import { Link, NavLink} from 'react-router-dom';
 
 class NavSide extends React.Component{
     render() {
@@ -13,52 +14,53 @@ class NavSide extends React.Component{
                 <div className="sidebar-collapse">
                     <ul className="nav" id="main-menu">
                         <li>
-                            <a className="active-menu" href="index.html"><i className="fa fa-dashboard"></i> Dashboard</a>
+                        {/* 选中状态下的className=“active-menu” */}
+                            <NavLink exact activeClassName="active-menu" to="/">
+                                <i className="fa fa-dashboard"></i>
+                                <span>首页</span>
+                            </NavLink>
                         </li>
-                        <li>
-                            <a href="ui-elements.html"><i className="fa fa-desktop"></i> UI Elements</a>
-                        </li>
-                        <li>
-                            <a href="chart.html"><i className="fa fa-bar-chart-o"></i> Charts</a>
-                        </li>
-                        <li>
-                            <a href="tab-panel.html"><i className="fa fa-qrcode"></i> Tabs &amp; Panels</a>
-                        </li>
-                        <li>
-                            <a href="table.html"><i className="fa fa-table"></i> Responsive Tables</a>
-                        </li>
-                        <li>
-                            <a href="form.html"><i className="fa fa-edit"></i> Forms </a>
-                        </li>
-                        <li>
-                            <a href="#"><i className="fa fa-sitemap"></i> Multi-Level Dropdown<span className="fa arrow"></span></a>
-                            <ul className="nav nav-second-level collapse">
+                        <li class="active">
+                            <Link to="/product">
+                                <i className="fa fa-sitemap"></i> 
+                                <span>商品</span>
+                                <span className="fa arrow"></span>
+                            </Link>
+                            {/* 收起的className="collapse" */}
+                            <ul className="nav nav-second-level collapse in">
                                 <li>
-                                    <a href="#">Second Level Link</a>
+                                    <NavLink to="/product" activeClassName="active-menu">商品管理</NavLink>
                                 </li>
                                 <li>
-                                    <a href="#">Second Level Link</a>
-                                </li>
-                                <li>
-                                    <a href="#">Second Level Link<span className="fa arrow"></span></a>
-                                    <ul className="nav nav-third-level collapse">
-                                        <li>
-                                            <a href="#">Third Level Link</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Link</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Link</a>
-                                        </li>
-
-                                    </ul>
-
+                                    <NavLink to="/product-category" activeClassName="active-menu">品类管理</NavLink>
                                 </li>
                             </ul>
                         </li>
-                        <li>
-                            <a href="empty.html"><i className="fa fa-fw fa-file"></i> Empty Page</a>
+                        <li class="active">
+                            <Link to="/order">
+                                <i className="fa fa-sitemap"></i> 
+                                <span>订单</span>
+                                <span className="fa arrow"></span>
+                            </Link>
+                            {/* 收起的className="collapse" */}
+                            <ul className="nav nav-second-level collapse in">
+                                <li>
+                                    <NavLink to="/order" activeClassName="active-menu">订单管理</NavLink>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="active">
+                            <Link to="/user">
+                                <i className="fa fa-sitemap"></i> 
+                                <span>用户</span>
+                                <span className="fa arrow"></span>
+                            </Link>
+                            {/* 收起的className="collapse" */}
+                            <ul className="nav nav-second-level collapse in">
+                                <li>
+                                    <NavLink to="/user" activeClassName="active-menu">用户管理</NavLink>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
 
