02d991704208f762db288cd8d284c11dfbb2ac1c
yuzexia <327932801@qq.com>
2021-05-18 01:53:40 +0800

    image upload, delete image

--- src/page/product/index/save.jsx
+++ src/page/product/index/save.jsx
@@ -2,7 +2,7 @@
  * @Author: yuze.xia 
  * @Date: 2021-05-11 13:54:08 
  * @Last Modified by: yuze.xia
- * @Last Modified time: 2021-05-12 16:56:13
+ * @Last Modified time: 2021-05-18 01:52:24
  */
 import React from 'react';
 
@@ -11,13 +11,47 @@ import Product from 'service/product-service.jsx';
 import FileUploader from 'util/file-upload/index.jsx';
 import PageTitle from 'component/page-title/index.jsx';
 import CategorySelector from './category-selector.jsx';
+
+import './save.less';
+
+const _mm = new MUtil();
+
 class ProductSave extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            categoryId          : 0,
+            parentCategoryId    : 0,
+            subImages           : []
+        }
     }
-
+    // 品类选择器的变化
     onCategoryChange(categoryId, parentCategoryId) {
         console.log('save::::', categoryId, parentCategoryId);
+    }
+
+    // 上传图片成功方法
+    onUploadSuccess(res){
+        let subImages = this.state.subImages;
+        subImages.push(res)
+        // 上传成功之后将图片插入到subImages中
+        this.setState({
+            subImages: subImages
+        })
+        console.log('uploadFilesSuccess::::', this.state);
+    }
+    // 上传图片失败方法
+    onUploadError(errMsg) {
+        _mm.errorTips(errMsg);
+    }
+    // 删除图片操作
+    onImageDelete(e) {
+        let index       = parseInt(e.target.getAttribute('index')),
+            subImages   = this.state.subImages;
+        subImages.splice(index, 1)
+        this.setState({
+            subImages: subImages
+        })
     }
     render() {
         return(
@@ -62,7 +96,21 @@ class ProductSave extends React.Component{
                     <div className="form-group">
                         <label className="col-md-2 control-label">商品图片</label>
                         <div className="col-md-10">
-                            <FileUploader />
+                            {
+                                this.state.subImages.length ? this.state.subImages.map((image, index) => {
+                                    return (
+                                        <div className="img-con" key={index}>
+                                            <img className="img" src={image.url}/>
+                                            <i className="fa fa-close" index={index} onClick={(e) => this.onImageDelete(e)}></i>
+                                        </div>
+                                    )
+                                })
+                                : (<div>请上传图片</div>)
+                            }
+                        </div>
+                        <div className="col-md-10 col-md-offset-2 file-upload-con">
+                            <FileUploader onSuccess={(res) => {this.onUploadSuccess(res)}}
+                                        onError={(errMsg) => {this.onUploadError(errMsg)}}/>
                         </div>
                     </div>
                     <div className="form-group">
--- src/util/file-upload/index.jsx
+++ src/util/file-upload/index.jsx
@@ -2,7 +2,7 @@
  * @Author: yuze.xia 
  * @Date: 2021-05-12 16:47:26 
  * @Last Modified by: yuze.xia
- * @Last Modified time: 2021-05-18 00:28:32
+ * @Last Modified time: 2021-05-18 01:13:19
  */
 import React from 'react';
 import FileUpload from './react-fileupload.jsx';
@@ -15,18 +15,18 @@ class FileUploader extends React.Component{
             fileFieldName   : 'upload_file',
             dataType        : 'json',
             chooseAndUpload : true,
-            uploadSuccess: (res) => {
-                console.log(res);
+            uploadSuccess   : (res) => {
+                this.props.onSuccess(res.data);
             },
-            uploadError: (err) => {
-                console.log(err);
+            uploadError     : (err) => {
+                this.props.onError(err.message || '图片上传失败');
             }
         }
         /*Use FileUpload with options*/
         /*Set two dom with ref*/
         return (
             <FileUpload options={options}>
-                <button ref="chooseAndUpload">请选择图片</button>
+                <button ref="chooseAndUpload" className="btn btn-xs btn-default">请选择图片</button>
             </FileUpload>
         )	        
     }
